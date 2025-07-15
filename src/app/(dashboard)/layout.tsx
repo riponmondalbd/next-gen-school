612d74c8fd9a4225dfdc4192b0a257fc9807c349
riponmondalbd <riponmondal149@gmail.com>
2025-07-15 23:47:17 +0600

    logo --> position logo and size, also make a padding for logo

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -9,10 +9,14 @@ export default function DashboardLayout({
   return (
     <div className="h-screen flex">
       {/* left navbar component */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200">
-        <Link href={"/"}>
-          <Image src="/assets/school.png" alt="logo" width={32} height={32} />
-          <span className="hidden lg:block">NextGenSchool</span>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-200 p-4">
+        <Link
+          href={"/"}
+          className="flex items-center justify-center lg:justify-start gap-2
+        "
+        >
+          <Image src="/assets/school.png" alt="logo" width={35} height={35} />
+          <span className="hidden lg:block">NextGen</span>
         </Link>
       </div>
       {/* right main component */}
