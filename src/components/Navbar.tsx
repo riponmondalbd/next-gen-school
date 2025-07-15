658702a4941e3cc9f2e0c03992d7a7dd36110a5a
riponmondalbd <riponmondal149@gmail.com>
2025-07-16 00:30:25 +0600

    add navbar to dashboard layout and also add children for main content, also try to make navbar components design

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,19 @@
+import Image from "next/image";
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="flex items-center justify-between gap-4">
+      {/* searchbar */}
+      <div className="hidden md:flex">
+        <Image
+          src="/assets/nav/search.png"
+          alt="search"
+          width={14}
+          height={14}
+        />
+      </div>
+      {/* icons and user */}
+      <div className="">icon and user</div>
+    </div>
+  );
 }
