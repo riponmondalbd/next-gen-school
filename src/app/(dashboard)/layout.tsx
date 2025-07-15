8805dc17c6f967f2da8cc5bb48bc8cfa0ec92630
riponmondalbd <riponmondal149@gmail.com>
2025-07-15 20:48:11 +0600

    dashboardLayout --> divide 2 part , make sizing content wise

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,5 +3,12 @@ export default function DashboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>dashboard{children}</div>;
+  return (
+    <div className="h-screen flex">
+      {/* left navbar component */}
+      <div className="w-1/6 bg-red-200">l</div>
+      {/* right main component */}
+      <div className="w-5/6 bg-blue-200">r</div>
+    </div>
+  );
 }
