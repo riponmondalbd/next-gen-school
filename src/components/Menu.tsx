d91e96aaee6208ec3958c725330dde23f2a5c1d3
riponmondalbd <riponmondal149@gmail.com>
2025-07-16 00:15:54 +0600

    styles parens and child navbar and also make responsive for all devices

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -118,14 +118,20 @@ const menuItems = [
 
 export default function Menu() {
   return (
-    <div>
+    <div className="mt-4 text-sm">
       {menuItems.map((item) => (
-        <div className="" key={item.title}>
-          <span>{item.title}</span>
+        <div className="flex flex-col gap-2" key={item.title}>
+          <span className="hidden lg:block text-gray-400 font-light my-4">
+            {item.title}
+          </span>
           {item.items.map((singleItem) => (
-            <Link href={singleItem.href} key={singleItem.label}>
+            <Link
+              href={singleItem.href}
+              key={singleItem.label}
+              className="flex items-center justify-center lg:justify-start gap-4 text-gray-500 py-2"
+            >
               <Image src={singleItem.icon} alt="" width={20} height={20} />
-              <span>{singleItem.label}</span>
+              <span className="hidden lg:block">{singleItem.label}</span>
             </Link>
           ))}
         </div>
