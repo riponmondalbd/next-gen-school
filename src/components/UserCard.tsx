5664953e18f883aff68ae828e8c04aa79ccf19db
riponmondalbd <riponmondal149@gmail.com>
2025-07-17 01:37:27 +0600

    userCard --> use background based on even and odd

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,3 +1,7 @@
 export default function UserCard({ type }: { type: string }) {
-  return <div className="rounded-2xl">UserCard</div>;
+  return (
+    <div className="rounded-2xl odd:bg-nextPurple even:bg-nextYellow p-4">
+      UserCard
+    </div>
+  );
 }
