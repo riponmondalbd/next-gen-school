7319adefd7126731537cdf140c5aa154d89efc8c
riponmondalbd <riponmondal149@gmail.com>
2025-07-17 01:20:46 +0600

    admin page--> make user card and implement to it in the admin page

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,10 +1,17 @@
+import UserCard from "@/components/UserCard";
+
 export default function AdminPage() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* left side of main content */}
-      <div className="">l</div>
+      <div className="w-full lg:w-2/3">
+        {/* user cards */}
+        <div className="">
+          <UserCard type="" />
+        </div>
+      </div>
       {/* right side of main content  */}
-      <div className="">r</div>
+      <div className="w-full lg:w-1/3">r</div>
     </div>
   );
 }
