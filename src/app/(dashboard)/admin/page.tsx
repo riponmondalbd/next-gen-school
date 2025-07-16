d87a24c8c9c0f0c6afcbc40360ac7b8f9bb9d2a9
riponmondalbd <riponmondal149@gmail.com>
2025-07-16 23:58:07 +0600

    admin page ---> divide it 2 section left and right

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,10 @@
 export default function AdminPage() {
-  return <div>AdminPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* left side of main content */}
+      <div className="">l</div>
+      {/* right side of main content  */}
+      <div className="">r</div>
+    </div>
+  );
 }
