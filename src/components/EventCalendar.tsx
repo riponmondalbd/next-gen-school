52f4fdb13bf87bb7f17579c89744098b54bf3b71
riponmondalbd <riponmondal149@gmail.com>
2025-07-17 06:02:59 +0600

    EventCalender --> make temporary make data

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -8,6 +8,28 @@ type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
+// temporary data
+const events = [
+  {
+    id: 1,
+    title: "Lorem ipsum dolor",
+    time: "12.00 PM - 2.00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 2,
+    title: "Lorem ipsum dolor",
+    time: "12.00 PM - 2.00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    id: 3,
+    title: "Lorem ipsum dolor",
+    time: "12.00 PM - 2.00 PM",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+];
+
 export default function EventCalendar() {
   const [value, onChange] = useState<Value>(new Date());
 
