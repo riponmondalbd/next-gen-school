7b45daed20ed1d2f0c1e3d693aa5cab014bb898b
riponmondalbd <riponmondal149@gmail.com>
2025-07-21 03:45:50 +0600

    teacherPage.tsx--> copy all from the student page and past that to the teacher page

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,3 +1,26 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalender";
+import EventCalendar from "@/components/EventCalendar";
+
 export default function TeacherPage() {
-  return <div>TeacherPage</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left content */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          {/* page schedule section header title */}
+          <h1 className="text-xl font-semibold">Schedule (4a)</h1>
+          <BigCalendar />
+        </div>
+      </div>
+
+      {/* right content */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        {/* event calender */}
+        <EventCalendar />
+        {/* announcement */}
+        <Announcements />
+      </div>
+    </div>
+  );
 }
