7704e2ddc53db968bf603e7f449d1e63514332a4
riponmondalbd <riponmondal149@gmail.com>
2025-07-21 02:54:37 +0600

    data.ts --> update the correct date, previously i add wrong date

--- src/components/BigCalender.tsx
+++ src/components/BigCalender.tsx
@@ -1,21 +1,30 @@
 "use client";
 import { calendarEvents } from "@/lib/data";
 import moment from "moment";
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { useState } from "react";
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 const localizer = momentLocalizer(moment);
 
-const BigCalendar = (props) => (
-  <div>
+const BigCalendar = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChangeView = (selectView: View) => {
+    setView(selectView);
+  };
+  return (
     <Calendar
       localizer={localizer}
       events={calendarEvents}
       startAccessor="start"
       endAccessor="end"
-      style={{ height: 500 }}
+      views={["work_week", "day"]}
+      view={view}
+      style={{ height: "98%" }}
+      onView={handleOnChangeView}
     />
-  </div>
-);
+  );
+};
 
 export default BigCalendar;
