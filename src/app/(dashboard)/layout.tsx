f5cefb1635be8cbcac3e8e2ab3a59a538c1a43c2
riponmondalbd <riponmondal149@gmail.com>
2025-08-01 22:35:04 +0600

    public --> add assets folder for all images dashboard->layout.tsx --> inside left navbar content add logo and title also make styling

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,3 +1,6 @@
+import Image from "next/image";
+import Link from "next/link";
+
 export default function DashoboardLayout({
   children,
 }: Readonly<{
@@ -6,7 +9,15 @@ export default function DashoboardLayout({
   return (
     <div className="h-screen flex">
       {/* left navabr */}
-      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-50">l</div>
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-50 p-4">
+        <Link
+          href={"/"}
+          className="flex items-center justify-center lg:justify-start gap-2"
+        >
+          <Image src="/assets/logo.png" alt="logo" width={35} height={35} />
+          <span className="hidden lg:block">Next Gen</span>
+        </Link>
+      </div>
       {/* right content */}
       <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-50">
         r{children}
