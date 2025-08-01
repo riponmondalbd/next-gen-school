33e08883b51c9d4e0598155f093d8c78142e83cd
riponmondalbd <riponmondal149@gmail.com>
2025-08-01 22:22:10 +0600

    dashoboard->layout.tsx --> make 2 pertition of for left side i will kipt navbar and right side for the main content, also make responsive the hole container

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -3,5 +3,14 @@ export default function DashoboardLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div>dasboard{children}</div>;
+  return (
+    <div className="h-screen flex">
+      {/* left navabr */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] bg-red-50">l</div>
+      {/* right content */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-50">
+        r{children}
+      </div>
+    </div>
+  );
 }
