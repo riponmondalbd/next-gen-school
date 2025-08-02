b4d65577fbd28d51ad450305af588843ca82f602
riponmondalbd <riponmondal149@gmail.com>
2025-08-02 22:28:43 +0600

    admin-> page.tsx --> make 2 pertision of the container for left and right side content and also make them responsive for all devices

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,3 +1,10 @@
 export default function AdminPage() {
-  return <div>Admin Page</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col md:flex-row">
+      {/* left content */}
+      <div className="w-full lg:w-2/3">l</div>
+      {/* right content */}
+      <div className="w-full lg:w-1/3">r</div>
+    </div>
+  );
 }
