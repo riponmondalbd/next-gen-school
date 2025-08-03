758f912db914a2ce6c4d19b8d15de9bb8bbf18b9
riponmondalbd <riponmondal149@gmail.com>
2025-08-03 21:44:00 +0600

    components->UserCars.tsx --> make a user card, admin->page.tsx --> make 4 user cards for students, teachers,parents and staffs

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,8 +1,18 @@
+import UserCard from "@/components/UserCard";
+
 export default function AdminPage() {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
       {/* left content */}
-      <div className="w-full lg:w-2/3">l</div>
+      <div className="w-full lg:w-2/3">
+        {/* user cards */}
+        <div className="flex gap-4 justify-between">
+          <UserCard type="student" />
+          <UserCard type="teacher" />
+          <UserCard type="parents" />
+          <UserCard type="staffs" />
+        </div>
+      </div>
       {/* right content */}
       <div className="w-full lg:w-1/3">r</div>
     </div>
