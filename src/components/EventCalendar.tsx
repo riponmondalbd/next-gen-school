62ac74bf12075afc55856aa46bcc44184ad9454b
riponmondalbd <riponmondal149@gmail.com>
2025-08-05 23:51:20 +0600

    EventCalender.tsx --> make events and also make that fully complete

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Image from "next/image";
 import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
@@ -35,7 +36,33 @@ export default function EventCalendar() {
 
   return (
     <div className="bg-white p-4 rounded-md">
+      {/* calender */}
       <Calendar onChange={onChange} value={value} />
+
+      {/* events */}
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image
+          src={"/assets/moreDark.png"}
+          alt="dark icon"
+          width={20}
+          height={20}
+        />
+      </div>
+      <div className="flex flex-col gap-4">
+        {events.map((event) => (
+          <div
+            className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-nextSky even:border-t-nextPurple"
+            key={event.id}
+          >
+            <div className="flex items-center justify-between">
+              <h1 className="font-semibold text-gray-600">{event.title}</h1>
+              <span className="text-gray-300 text-xs">{event.time}</span>
+            </div>
+            <p className="mt-2 text-gray-400 text-sm">{event.description}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
