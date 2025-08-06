9d248697a0ed5b5f5c55674c68877f7ee8984027
riponmondalbd <riponmondal149@gmail.com>
2025-08-06 06:22:52 +0600

    copy the hole item's from the student page and past that to the teacher page

--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,3 +1,26 @@
+import Announcements from "@/components/Announcements";
+import BigCalendar from "@/components/BigCalendar";
+import EventCalendar from "@/components/EventCalendar";
+
 export default function TeacherPage() {
-  return <div>Teacher Page</div>;
+  return (
+    <div className="p-4 flex gap-4 flex-col xl:flex-row">
+      {/* left content */}
+      <div className="w-full xl:w-2/3">
+        <div className="h-full bg-white p-4 rounded-md">
+          <h1 className="text-xl font-semibold">Schedule (4A)</h1>
+          {/* big calender */}
+          <BigCalendar />
+        </div>
+      </div>
+      {/* right content */}
+      <div className="w-full xl:w-1/3 flex flex-col gap-8">
+        {/* calender and events */}
+        <EventCalendar />
+
+        {/* announcements */}
+        <Announcements />
+      </div>
+    </div>
+  );
 }
