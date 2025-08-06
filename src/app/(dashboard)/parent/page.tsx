fe3e56ed50e313f503f4bd75c5ef8c4cde1efe74
riponmondalbd <riponmondal149@gmail.com>
2025-08-07 04:28:19 +0600

    copy all from teacher page and pest it to parent page and each parents have multiple children so, i have to make schedule for each children

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -7,7 +7,7 @@ export default function ParentPage() {
       {/* left content */}
       <div className="w-full xl:w-2/3">
         <div className="h-full bg-white p-4 rounded-md">
-          <h1 className="text-xl font-semibold">Schedule</h1>
+          <h1 className="text-xl font-semibold">Schedule (John Doe)</h1>
           {/* big calender */}
           <BigCalendar />
         </div>
