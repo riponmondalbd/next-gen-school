5550ae334b36b41b7b83d6ad814a4ea35d3da2d8
riponmondalbd <riponmondal149@gmail.com>
2025-08-08 03:53:32 +0600

    copy all from the TeachersListPage and pest that to StudentListPage

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,7 +4,9 @@ export default function Table({
   data,
 }: {
   columns: { header: string; accessor: string; className?: string }[];
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   renderRow: (item: any) => React.ReactNode;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   data: any[];
 }) {
   return (
