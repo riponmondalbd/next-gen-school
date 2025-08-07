12d4f8a49be16520a16128b33d1b57cb1b09975a
riponmondalbd <riponmondal149@gmail.com>
2025-08-08 05:54:46 +0600

    make dynamic teachers page for each teachers details, and make 2 section left and right and also declare there size

---
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -0,0 +1,10 @@
+export default function SingleTeacherPage() {
+  return (
+    <div className="flex-1 p-4 flex flex-col xl:flex-row gap-4">
+      {/* left content */}
+      <div className="w-full xl:w-2/3">l</div>
+      {/* right content */}
+      <div className="w-full xl:w-1/3">r</div>
+    </div>
+  );
+}
