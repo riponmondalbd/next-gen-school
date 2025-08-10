f378266633620a7101b2186717069340729a0bff
riponmondalbd <riponmondal149@gmail.com>
2025-08-11 05:22:22 +0600

    make modal close option

--- src/components/FormModel.tsx
+++ src/components/FormModel.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import { useState } from "react";
 
 export default function FormModel({
   table,
@@ -33,13 +34,38 @@ export default function FormModel({
       : type === "update"
       ? "bg-nextSky"
       : "bg-nextPurple";
+
+  const [open, setOpen] = useState(false);
+
   return (
     <>
       <button
         className={`${size} flex items-center justify-center rounded-full cursor-pointer ${bgColor}`}
+        // trigger modal
+        onClick={() => setOpen(true)}
       >
         <Image src={`/assets/${type}.png`} alt="" width={16} height={16} />
       </button>
+
+      {/* modal */}
+      {open && (
+        <div className="w-screen h-screen absolute left-0 top-0 bg-black/60 z-50 flex items-center justify-center">
+          <div className="bg-white rounded-md p-4 relative w-[90%] md:w-[70%] lg:w-[60%] xl:w-[50%] 2xl:w-[40%]">
+            {/* close modal icon button */}
+            <div
+              className="absolute top-4 right-4 cursor-pointer"
+              onClick={() => setOpen(false)}
+            >
+              <Image
+                src="/assets/close.png"
+                alt={"close icon"}
+                width={14}
+                height={14}
+              />
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
